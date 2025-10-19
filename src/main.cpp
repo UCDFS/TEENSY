4f32c772cf36ee186f1dfe511b255539166631bb
Charlie Zhang <charliezhang2004@hotmail.com>
2025-10-19 20:19:10 +0100

    Refactor setup and loop functions to include serial initialization and brake light control

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
+#include "brake_light.h"
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  unsigned long t0 = millis();
+  while (!Serial && (millis() - t0) < 1500) { } // wait for serial up to 1.5s
+  brake_light_setup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  brake_light_update();
 }
