96f8f147f28ca9f3620f538757988a6cb977eef4
Louis Brennan <louis.brennan1@ucdconnect.ie>
2026-03-29 18:41:26 +0100

    Created Logger class using a circular buffer

---
+++ include/CircularBuffer.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "config.h"
+
+template <typename T, size_t size>
+class CircularBuffer {
+private:
+  T buffer[size];
+  size_t _head = 0;
+  size_t _tail = 0;
+  bool _full = false;
+
+public:
+  void push(T item) {
+    _buffer[_head] = item;
+    if (_full) {
+      _tail = (_tail + 1) % size;
+    }
+    _head = (_head + 1) % size;
+    _full = _head == _tail;
+  }
+
+  bool pop (&T item) {
+    if (isEmpty()) {
+      return false;
+    }
+
+    item = _buffer[_tail];
+    _full = false;
+    _tail = (_tail + 1) % size;
+    return true;
+  }
+
+  bool isEmpty() const { return (!_full && (_head == _tail)); }
+  bool isFull() const { return _full; }
+};
---
+++ include/config.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <SdFat.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+
+
+// --------- LOGGING ----------
+#define LOG_SD true
+#define LOG_SERIAL false
+#define MAX_BUF 16
+#define MAX_LOG_LEN 128
+extern File logFile; 
